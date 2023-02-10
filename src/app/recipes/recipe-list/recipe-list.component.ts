0a240ed37443a32e12156a7d002d283da1df05d7
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2023-02-10 17:54:49 +0530

    Add practice

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Adilakshmi", "Adilakshmi Recipe", "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2023/1/03/fnk_football_cheese_ball_and_charcuterie_board_s4x3.jpg.rend.hgtvcom.406.271.suffix/1672860421438.jpeg")
+  ]
   constructor() { }
 
   ngOnInit(): void {
